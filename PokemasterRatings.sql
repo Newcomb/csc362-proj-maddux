c2816726764be1570fb4bbe10d2295a700195856
Newcomb P. Maddux <newcomb.maddux@centre.edu>
2021-11-01 19:44:01 +0000

    add deletion rule

--- PokemasterRatings.sql
+++ PokemasterRatings.sql
@@ -3,6 +3,7 @@ CREATE TABLE pokemaster_ratings (
     pokemaster_id           INT,
     move_id                 INT,
     star_rating             INT,
-    FOREIGN KEY (move_id) REFERENCES moves (move_id),
+    FOREIGN KEY (move_id) REFERENCES moves (move_id)
+    ON DELETE RESTRICT,
     CONSTRAINT pokemaster_ratings_id PRIMARY KEY (pokemaster_id, move_id)
 );
