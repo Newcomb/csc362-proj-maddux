148ef13873ccdb2fdec1624f1fff9357a2e3b8e5
Josh West <josh.west@centre.edu>
2021-11-01 10:26:14 -0400

    schedule.sql created

---
+++ Schedule.sql
@@ -0,0 +1,16 @@
+/*Make sure the correct db is being used*/
+USE pokemon_db;
+
+/*Make the table*/
+CREATE TABLE Schedule (
+    MoveID              INT NOT NULL,
+    WhenTaught          DATETIME,
+    TeachingDuration    INT NOT NULL,
+    Offered             BIT NOT NULL,
+    FOREIGN KEY MoveID REFERENCES Moves.MoveID
+);
+
+/*Fill the table with values (according to national pokedex)
+link: https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number*/
+/*INSERT INTO Schedule (MoveID, WhenTaught, TeachingDuration, Offered)
+VALUES();*/
