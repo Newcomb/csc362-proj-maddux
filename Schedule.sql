221f53ea01d9918001cb83027294b892a6f14097
Josh West <josh.west@centre.edu>
2021-11-03 15:41:43 -0400

    updated comments

--- Schedule.sql
+++ Schedule.sql
@@ -18,8 +18,3 @@ CREATE TABLE schedule (
     offered              BIT NOT NULL,
     FOREIGN KEY move_id REFERENCES moves (move_id) ON DELETE RESTRICT
 );
-
-/*Fill the table with values (according to national pokedex)
-link: https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number*/
-/*INSERT INTO Schedule (MoveID, WhenTaught, TeachingDuration, Offered)
-VALUES();*/
