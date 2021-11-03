f4af81dd6c062b786ec7e381756cb30d9528f447
Aj Howell <aj.howell@centre.edu>
2021-11-03 19:17:06 +0000

    Add new file

---
+++ KnownMoves.sql
@@ -0,0 +1,12 @@
+CREATE TABLE known_moves
+(
+    ownedpokemon_id INT NOT NULL,
+    move_id INT AUTO_INCREMENT NOT NULL,
+
+    FOREIGN KEY moves REFERENCES moves(move_id),
+    FOREIGN KEY owned_pokemon REFERENCES owned_pokemon(owned_pokemon_id),
+    
+    -- Setting up primary key
+    PRIMARY KEY (move_id, owned_pokemon_id);
+
+)
