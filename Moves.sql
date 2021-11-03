10deb76cf9b5fab000261ba8173bb7779ad57eb0
Aj Howell <aj.howell@centre.edu>
2021-11-03 19:23:21 +0000

    Moves table uploaded

---
+++ Moves.sql
@@ -0,0 +1,15 @@
+
+-- creating the moves table
+
+CREATE TABLE moves
+(
+    moveID INT AUTO_INCREMENT NOT NULL,
+    move_name VARCHAR(50) NOT NULL,
+    --move_type VARCHAR (50) NOT NULL,
+    type_id INT NOT NULL AUTO_INCREMENT,
+    hidden_move VARCHAR (10) NOT NULL,
+    -- not sure what the status is again check in with group
+    taught_status  VARCHAR (11) NOT NULL,
+    FOREIGN KEY (Types) REFERENCES Types(type_id),
+    PRIMARY KEY(move_id);
+)
