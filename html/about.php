9a5413f529017fde61d293c9d1c919dfc9ce6151
Josh West <josh.west@centre.edu>
2021-11-14 01:06:50 +0000

    added about page

---
+++ html/about.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="stylesheet" href="menu.css">
+</head>
+<body>
+<h1>About</h1>
+<div id=about> 
+        <p>Bill's Move Tutoring TM has been a trusted training service for over 20 years. 
+            Serving over 1 million trainers and 6 million pokemon, Bill has established himself as a trustworthy businessman. 
+            <br>
+            Please reach out to him at billboss@aol.com for more information or schedule his services on this site today.
+        </p>
+    </div>
+</body>
+</html>
--- html/menu.php
+++ html/menu.php
@@ -49,13 +49,7 @@
         </div>
     </div>
     <a href="#contact">Rate My Services</a>
-    <a href="#about">About</a>
-    </div>
-    <div id=about> 
-        <p>Bill's Move Tutoring TM has been a trusted training service for over 20 years. 
-            Serving over 1 million trainers and 6 million pokemon, Bill has established himself as a trustworthy businessman. 
-            Please reach out to him at billboss@aol.com for more information or schedule his services on this site today.
-        </p>
+    <a href="about.php">About</a>
     </div>
     <?php
     
