c42449115b4dd975dde10a6f515c4b48159ba403
Josh West <josh.west@centre.edu>
2021-11-15 22:05:57 +0000

    created viewTable; made pokedex view page

---
+++ html/viewTable.php
@@ -0,0 +1,52 @@
+<?php
+function viewTable($res,$path) {
+    // Get array of table vals set up
+    $num_rows = $res->num_rows;
+    $num_cols = $res->field_count;
+    $resar = $res->fetch_all();
+    ?>
+    <p>
+    <?php echo $num_cols; ?> columns, <?php echo $num_rows; ?> rows.
+    </p>
+    <form action=<?php echo $path ?> method=POST>
+        <table>
+        <thead>
+        <tr>
+    <?php
+    // loop that writes the table headers
+    while ($fld = $res->fetch_field()) {
+    ?>
+        <th><?php echo $fld->name; ?></th>
+    <?php
+    }
+    ?>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    // Loop for the rows of the table
+    for ($i=0;$i<$num_rows; $i++) {
+    ?>
+    <tr>
+    <?php
+    // Loop for the columns of the table
+        for ( $j = 0; $j < $num_cols; $j++ ) {
+            // Prints the value of a table
+    ?>
+            <td><?php echo $resar[$i][$j]; ?></td>
+    <?php
+        }
+    ?>
+    </tr>
+    <?php
+    }
+?>
+    </tbody>
+    </table>
+    </form>
+    <!-- <form action=<?php// echo $path ?> method=POST>
+        <input type="submit" name="toggle" value="Toggle Light/Dark Mode"/>
+    </form>  -->
+<?php
+}
+?>
