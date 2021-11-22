72b97a84fd8b5ab60d099dcb29b355bdec53e204
newcomb <newcomb.maddux@centre.edu>
2021-11-22 19:59:46 +0000

    add moves

--- Tables/Moves.sql
+++ Tables/Moves.sql
@@ -3,8 +3,8 @@ CREATE TABLE moves
     PRIMARY KEY (move_id),
     move_id         INT NOT NULL AUTO_INCREMENT,
     move_name       VARCHAR(50) NOT NULL,
-    hidden_move     VARCHAR(10) NOT NULL,
-    taught_status   VARCHAR(11) NOT NULL,
+    hidden_move     TINYINT(1) NOT NULL,
+    taught_status   TINYINT(1) NOT NULL,
     type_id         INT NOT NULL,
     FOREIGN KEY (type_id) REFERENCES types (type_id) ON DELETE RESTRICT
 )
--- Tables/TableValues.sql
+++ Tables/TableValues.sql
@@ -68,3 +68,13 @@ VALUES  (1, 4),
         (4, 11),
         (4, 8);
 
+INSERT INTO moves (move_name, hidden_move, taught_status, type_id)
+VALUES  ('Absorb', 0, 0, 4),
+        ('Acid', 0, 0, 8),
+        ('Bite', 0, 0, 15),
+        ('Blizzard', 0, 0, 6),
+        ('Body Slam', 0, 0, 1),
+        ('Charge Beam', 0, 0, 5),
+        ('Curse', 0, 0, 14),
+        ('Dive', 1, 0, 3),
+        ('Dragon Breath', 0, 0, 15);
