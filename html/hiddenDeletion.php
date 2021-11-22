18f720b6c86a5fc552455c24f86057c3bd2ef861
Josh West <josh.west@centre.edu>
2021-11-22 18:16:15 +0000

    updated menu; view schedule; added hiddenDeletion

---
+++ html/hiddenDeletion.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<link rel="stylesheet" href="menu.css">
+</head>
+<body>
+
+<div class=menu>
+<?php
+//Create the menu on this page 
+include "menu.php";
+?>
+</div>
+
+<h1>Hidden Move Deletion</h1>
+<div id=about> 
+        <p>Bill's Move Tutoring TM does not offer hidden move deletion.  
+            <br>
+            Please reach out to Bill at billboss@aol.com for reccomendations on trusted hidden move deletion services.
+        </p>
+</div>
+
+</body>
+</html>
--- html/menu.php
+++ html/menu.php
@@ -21,23 +21,11 @@
             <div class="subnav">
                 <button class="subnavbtn">Services <i class="fa fa-caret-down"></i></button>
                 <div class="subnav-content">
-                    <a href="#train">Train</a>
-                    <a href="#move-deletion">Move Deletion</a>
+                    <a href="hiddenDeletion.php">Hidden Move Deletion</a>
                     <a href="managePokemonTable.php">Manage All Pokemon</a>
                     <a href="manageOwnedPokemonTable.php">Manage Owned Pokemon</a>
-                        <div class="dropdown">
-                            <button class="dropbtn" id="managedb">Manage Database Pages
-                            <i class="fa fa-caret-down"></i>
-                            </button>
-                            <div class="dropdown-content">
-                                <a href="pokedexTable.php">Pokedex Table</a>
-                                <a href="pokemasterTable.php">Pokemaster Table</a>
-                                <a href="pokemasterRatingsTable.php">Pokemaster Ratings Table</a>
-                                <a href="movesTable.php">Moves Table</a>
-                                <a href="typesTable.php">Types Table</a>
-                            </div>
-                        </div>
-                        <div class="dropdown">
+
+                    <div class="dropdown">
                             <button class="dropbtn" id="viewdb">View Database Pages
                             <i class="fa fa-caret-down"></i>
                             </button>
@@ -49,12 +37,26 @@
                                 <a href="viewTypesTable.php">Types Table</a>
                             </div>
                         </div> 
+
+                    <div class="dropdown">
+                        <button class="dropbtn" id="managedb">Manage Database Pages
+                        <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="pokedexTable.php">Pokedex Table</a>
+                            <a href="pokemasterTable.php">Pokemaster Table</a>
+                            <a href="pokemasterRatingsTable.php">Pokemaster Ratings Table</a>
+                            <a href="movesTable.php">Moves Table</a>
+                            <a href="typesTable.php">Types Table</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="subnav">
                 <button class="subnavbtn">Schedule <i class="fa fa-caret-down"></i></button>
                 <div class="subnav-content">
-                    <a href="#move-schedule">Move Schedule</a>
+                    <a href="viewSchedule.php">View Schedule</a>
+                    <a href="manageSchedule.php">Manage Schedule</a>
                 </div>
             </div>
                 <a href="rateMyServices.php">Rate Bill's Services</a>
