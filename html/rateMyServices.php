fee7c9b59b4493de213d0e221321c0049a2e2f80
Minh Vu <minh.vu@centre.edu>
2021-11-29 21:34:32 +0000

    test

--- html/rateMyServices.php
+++ html/rateMyServices.php
@@ -54,7 +54,7 @@ error_reporting(E_ALL);
     <h1>Rate My Services</h1>
     <h3>Give new rating</h3>
     <form method="POST" action='rateMyServices.php'>
-            <?php drop_down_options('/DML/ViewPokemasters.sql', 0, $sql_path, 'Choose  ID', 'pokemasterID'); ?> 
+            <?php drop_down_options('/DML/ViewPokemasters.sql', 0, $sql_path, 'Choose PokemasterID', 'pokemasterID'); ?>
             <?php drop_down_options('/DML/ViewMoves.sql', 0, $sql_path, 'Choose Move ID', 'moveID'); ?>
             <label>Choose star rating</label>
             <select name = ratings>
