8861da5433b11cced96e5a9d8721c8311031078f
newcomb <newcomb.maddux@centre.edu>
2021-11-29 15:38:16 +0000

    Add primary keys to cpk tables

--- Tables/KnownMoves.sql
+++ Tables/KnownMoves.sql
@@ -1,9 +1,10 @@
 CREATE TABLE known_moves
 (
+    PRIMARY KEY (known_moves_id),
+    known_moves_id      INT NOT NULL AUTO_INCREMENT,
     owned_pokemon_id    INT NOT NULL,
     move_id             INT NOT NULL,
     FOREIGN KEY (owned_pokemon_id) REFERENCES owned_pokemon(owned_pokemon_id),
     FOREIGN KEY (move_id) REFERENCES moves(move_id),
-    PRIMARY KEY (move_id, owned_pokemon_id)
-
+    UNIQUE (owned_pokemon_id, move_id)
 );
--- Tables/PokemasterRatings.sql
+++ Tables/PokemasterRatings.sql
@@ -1,9 +1,13 @@
 -- SQL for creating pokemaster ratings table
 CREATE TABLE pokemaster_ratings (
-    pokemaster_id           INT,
-    move_id                 INT,
+    PRIMARY KEY (pokemaster_ratings_id),
+    pokemaster_ratings_id   INT NOT NULL AUTO_INCREMENT,
+    pokemaster_id           INT NOT NULL,
+    move_id                 INT NOT NULL,
     star_rating             INT CHECK (star_rating > 0 AND star_rating < 6),
     FOREIGN KEY (move_id) REFERENCES moves (move_id)
     ON DELETE RESTRICT,
-    CONSTRAINT pokemaster_ratings_id PRIMARY KEY (pokemaster_id, move_id)
+    FOREIGN KEY (pokemaster_id) REFERENCES pokemasters (pokemaster_id) 
+    ON DELETE RESTRICT,
+    UNIQUE (pokemaster_id, move_id)
 );
