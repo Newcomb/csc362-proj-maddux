f4247ce5398ea33b0e6d0a64affd5912e905c070
newcomb <newcomb.maddux@centre.edu>
2021-11-29 16:36:21 +0000

    Fix delete on known moves

--- DML/DeleteKnownMoves.sql
+++ DML/DeleteKnownMoves.sql
@@ -1,2 +1,2 @@
 -- Allow for move deletion from known moves populating with php.
-DELETE FROM known_moves WHERE (owned_pokemon_id = ? AND move_id = ?);
+DELETE FROM known_moves WHERE (known_move_id = ?);
