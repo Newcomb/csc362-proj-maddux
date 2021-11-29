878747aafa9dab84f019bc62ac6510aa83f8975b
Minh Vu <minh.vu@centre.edu>
2021-11-29 22:18:26 +0000

    finish rateMyServices

--- Tables/PokemasterRatings.sql
+++ Tables/PokemasterRatings.sql
@@ -5,7 +5,9 @@ CREATE TABLE pokemaster_ratings (
     pokemaster_id           INT NOT NULL,
     move_id                 INT NOT NULL,
     star_rating             INT CHECK (star_rating > 0 AND star_rating < 6),
-    FOREIGN KEY (move_id) REFERENCES moves (move_id),
-    FOREIGN KEY (pokemaster_id) REFERENCES pokemasters (pokemaster_id),
-    -- UNIQUE (pokemaster_id, move_id),
+    FOREIGN KEY (move_id) REFERENCES moves (move_id)
+    ON DELETE RESTRICT,
+    FOREIGN KEY (pokemaster_id) REFERENCES pokemasters (pokemaster_id) 
+    ON DELETE RESTRICT,
+    UNIQUE (pokemaster_id, move_id)
 );
