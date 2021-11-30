1120767543c61302edae8bbc8b174a03193a2ae6
aj.howell <aj.howell@centre.edu>
2021-11-30 02:48:17 +0000

    spelling mistake fixed

--- Tables/RatingCounts.sql
+++ Tables/RatingCounts.sql
@@ -2,7 +2,7 @@
 CREATE TABLE rating_counts (
     PRIMARY KEY (rating_count_id),
     rating_count_id     INT NOT NULL AUTO_INCREMENT,
-    raing_count         INT,
+    rating_count         INT,
     move_id             INT NOT NULL,
     FOREIGN KEY (move_id) REFERENCES moves (move_id)
     ON DELETE RESTRICT
