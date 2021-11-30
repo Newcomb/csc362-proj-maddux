4819633e6a0fd6931bcdca613454f5341022f6a5
Minh Vu <minh.vu@centre.edu>
2021-11-30 22:35:42 +0000

    Add delete to PokemasterRatings

--- html/rateMyServices.php
+++ html/rateMyServices.php
@@ -31,7 +31,7 @@ error_reporting(E_ALL);
     if (isset($_POST['SubmitRating'])) {
         // Prepare the delete statement
         $stmt = $conn->prepare(file_get_contents($sql_path . "/DML/InsertPokemasterRatings.sql"));
-        $stmt->bind_param('iii', intval($_POST['pokemasterID']), intval($_POST['moveName']), intval($_POST['ratings']));
+        $stmt->bind_param('iii', intval($_POST['pokemasterID']), intval($_POST['moveID']), intval($_POST['ratings']));
         $stmt->execute();
         header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
         die();
@@ -45,11 +45,11 @@ error_reporting(E_ALL);
     ?>
 </head>
 <body>
-    <h1>Rate Bill's Services</h1>
+    <h1>Rate My Services</h1>
     <h3>Give new rating</h3>
     <form method="POST" action='rateMyServices.php'>
             <?php drop_down_options('/DML/ViewPokemasters.sql', 0, $sql_path, 'Choose PokemasterID', 'pokemasterID'); ?>
-            <?php drop_down_options('/DML/ViewMoves.sql', 0, $sql_path, 'Choose Move Name', 'moveName'); ?>
+            <?php drop_down_options('/DML/ViewMoves.sql', 0, $sql_path, 'Choose Move Name', 'moveID'); ?>
             <label>Choose star rating</label>
             <select name = ratings>
                 <option value = '1'>1</option>
